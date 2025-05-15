4538367e955ea42c2d9713e68a35a66fffd2f7e5
Jerusalem Hailu <hailus02@gmail.com>
2025-05-15 17:10:11 -0400

    added env file to gitignore

--- Netflix-clon/src/Pages/Home/Home.jsx
+++ Netflix-clon/src/Pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <>
                 < Header/>
               <Banner />
-              {/* <RowList /> */}
+              <RowList />
                  <Footer /> 
             </>
         );
